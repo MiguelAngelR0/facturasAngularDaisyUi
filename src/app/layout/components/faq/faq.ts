b700aa5fb2fb6095b1dd9f16794e2377267c9f98
MiguelAngelR0 <miguelangelrodriguezescorcia@gmail.com>
2025-09-03 18:06:12 +0200

    animaciones

--- src/app/layout/components/faq/faq.ts
+++ src/app/layout/components/faq/faq.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
+import AOS from 'aos';
 
 @Component({
   selector: 'app-faq',
@@ -11,6 +12,14 @@ export class Faq {
   openFaq: number | null = null;
   toggleFaq(index: number) {
     this.openFaq = this.openFaq === index ? null : index;
+  }
+
+  ngAfterViewInit(): void {
+    AOS.init({
+      duration: 1000,
+      easing: 'ease-in-out',
+      once: false
+    });
   }
 
 }
