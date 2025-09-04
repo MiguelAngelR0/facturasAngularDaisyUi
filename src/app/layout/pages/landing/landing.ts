e6e5f7da56f8630406c3f86e48cc807bdaa7ad44
MiguelAngelR0 <miguelangelrodriguezescorcia@gmail.com>
2025-09-04 20:22:51 +0200

    about us

--- src/app/layout/pages/landing/landing.ts
+++ src/app/layout/pages/landing/landing.ts
@@ -5,10 +5,11 @@ import { Footer } from "../../../shared/components/footer/footer";
 import { Price } from "../../components/price/price";
 import { Faq } from "../../components/faq/faq";
 import { Contact } from "../../components/contact/contact";
+import { About } from "../../components/about/about";
 
 @Component({
   selector: 'app-landing',
-  imports: [Hero, Nav, Footer, Price, Faq, Contact],
+  imports: [Hero, Nav, Footer, Price, Faq, Contact, About],
   templateUrl: './landing.html',
   styles: ``
 })
